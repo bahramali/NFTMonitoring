42dde18f85a9fa767b26ba5bb8355ce9456b2855
keyvankianian <kianian.keyvan@email.com>
2025-07-14 22:04:07 +0200

    run test

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
+    test: {
+        globals: true,             // ✅ expect رو global میکنه
+        environment: 'jsdom'       // ✅ برای اجرای React در DOM مجازی
+    }
 });
