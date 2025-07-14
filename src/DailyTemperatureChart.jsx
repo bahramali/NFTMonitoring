b8d985664e749ca48c397a5905a4199da241288f
Azad <bahramali.az@gmail.com>
2025-07-15 01:34:13 +0200

    Persist daily data in localStorage and show hourly axis

---
+++ src/DailyTemperatureChart.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    ResponsiveContainer,
+    Label
+} from 'recharts';
+
+const DailyTemperatureChart = ({ data }) => (
+    <div style={{ width: '100%', height: 300 }}>
+        <ResponsiveContainer>
+            <LineChart data={data} margin={{ top: 20, right: 30, left: 0, bottom: 50 }}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis
+                    dataKey="time"
+                    type="number"
+                    domain={[0, 23]}
+                    ticks={[...Array(24).keys()]}
+                    tickFormatter={h => String(h).padStart(2, '0')}
+                    interval={0}
+                />
+                <YAxis>
+                    <Label value="°C" angle={-90} position="insideLeft" />
+                </YAxis>
+                <Tooltip />
+                <Line type="monotone" dataKey="temperature" stroke="#ff7300" dot={false} />
+            </LineChart>
+        </ResponsiveContainer>
+    </div>
+);
+
+export default DailyTemperatureChart;
