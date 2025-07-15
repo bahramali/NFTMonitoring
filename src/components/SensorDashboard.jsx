f1905f72e3202a30071ba2134a19556be6828343
Azad <bahramali.az@gmail.com>
2025-07-15 17:46:25 +0200

    Disable chart animation

--- src/components/SensorDashboard.jsx
+++ src/components/SensorDashboard.jsx
@@ -130,7 +130,11 @@ function SensorDashboard() {
             <h1>🌿 AzadFarm - Sensor & Camera Dashboard</h1>
 
             <ResponsiveContainer width="100%" height={400}>
-                <BarChart data={spectrumData} margin={{ top: 20, right: 30, left: 0, bottom: 50 }}>
+                <BarChart
+                    data={spectrumData}
+                    margin={{ top: 20, right: 30, left: 0, bottom: 50 }}
+                    isAnimationActive={false}
+                >
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="name" angle={-30} textAnchor="end" interval={0} height={60} />
                     <YAxis>
