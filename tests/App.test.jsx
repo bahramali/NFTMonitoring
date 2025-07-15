67c6995d9e9532b6607c1f7cfe947f585e34b6a0
Azad <bahramali.az@gmail.com>
2025-07-15 16:54:50 +0200

    Filter sensor noise and add tests

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import App from './App';
+import App from '../src/App';
 
 test('renders App component', () => {
     const { container } = render(<App />);
