9e4fa1ac940f51ec8f6730ba26ae28e5bbdfaa5b
Azad <bahramali.az@gmail.com>
2025-07-15 16:37:16 +0200

    Freeze daily charts until filter applied

---
+++ tests/MultiBandChart.test.jsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import MultiBandChart from '../src/components/MultiBandChart';
+
+test('renders multi-band line chart', () => {
+    const data = [
+        { time: 0, F1: 1, F2: 2 },
+        { time: 1, F1: 2, F2: 3 }
+    ];
+    const { container } = render(<MultiBandChart data={data} />);
+    const svg = container.querySelector('svg');
+    expect(svg).toBeInTheDocument();
+});
