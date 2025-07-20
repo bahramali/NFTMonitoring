d91d0c45abacefe54746f03326c796fb0798d7b1
bahramali <bahramali.az@gmail.com>
2025-07-20 08:42:54 +0200

    delete logs

--- src/components/SensorDashboard.jsx
+++ src/components/SensorDashboard.jsx
@@ -36,7 +36,6 @@ function SensorDashboard() {
     });
     const [dailyData, setDailyData] = useState(() => {
         const stored = localStorage.getItem("dailyData");
-        console.log("Stored: "+ stored);
         const now = Date.now();
         const initial = stored ? trimOldEntries(JSON.parse(stored), now, 30 * 24 * 60 * 60 * 1000) : [];
         localStorage.setItem("dailyData", JSON.stringify(initial));
@@ -131,8 +130,7 @@ function SensorDashboard() {
         client.on("message", (t, message) => {
             if (t === topic) {
                 try {
-                    console.log("message: "+message);
-                    console.log(message.toString());
+                    console.log("message.toString(): "+ message.toString());
                     const raw = parseSensorJson(message.toString());
                     const normalized = normalizeSensorData(raw);
                     const cleaned = filterNoise(normalized);
