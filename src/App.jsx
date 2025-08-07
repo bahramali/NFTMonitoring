e8fb07c8236d1470ab4066ef3f3eb71b338b2198
Azad <bahramali.az@gmail.com>
2025-08-07 16:59:07 +0200

    Add router structure and placeholder pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import SensorDashboard from './components/SensorDashboard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import Dashboard from './pages/Dashboard';
+import Reports from './pages/Reports';
+import Settings from './pages/Settings';
+import UserInfo from './pages/UserInfo';
+import Documentation from './pages/Documentation';
+import Device from './pages/filters/Device';
+import Layer from './pages/filters/Layer';
+import System from './pages/filters/System';
 
 function App() {
     return (
-        <div>
-            <SensorDashboard />
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={<Dashboard />} />
+                    <Route path="reports" element={<Reports />} />
+                    <Route path="settings" element={<Settings />} />
+                    <Route path="user" element={<UserInfo />} />
+                    <Route path="docs" element={<Documentation />} />
+                    <Route path="filters/device" element={<Device />} />
+                    <Route path="filters/layer" element={<Layer />} />
+                    <Route path="filters/system" element={<System />} />
+                </Route>
+            </Routes>
+        </Router>
     );
 }
 
---
+++ src/pages/filters/Layer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Layer() {
+    return (
+        <div>
+            Layer Filter
+        </div>
+    );
+}
+
+export default Layer;
