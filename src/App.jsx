f43f9746fb87f6fc1bf4934e1f2445c4da115f3b
Azad <bahramali.az@gmail.com>
2025-08-21 23:00:28 +0200

    chore: remove unused filter pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,6 @@ import ReportsPage from './pages/ReportsPage';
 import Settings from './pages/Settings';
 import UserInfo from './pages/UserInfo';
 import Documentation from './pages/Documentation';
-import Device from './pages/filters/Device';
-import Layer from './pages/filters/Layer';
-import System from './pages/filters/System';
 
 function App() {
     const rawBase = import.meta?.env?.BASE_URL || '/';
@@ -25,9 +22,6 @@ function App() {
                     <Route path="settings" element={<Settings />} />
                     <Route path="user" element={<UserInfo />} />
                     <Route path="docs" element={<Documentation />} />
-                    <Route path="filters/device" element={<Device />} />
-                    <Route path="filters/layer" element={<Layer />} />
-                    <Route path="filters/system" element={<System />} />
                 </Route>
             </Routes>
         </BrowserRouter>
