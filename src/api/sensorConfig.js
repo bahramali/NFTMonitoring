f5d422471c26bd555c1b9dd4c1046a0921dceaed
Azad Bahramali <bahramali.az@gmail.com>
2025-09-03 17:39:45 +0200

    fixing the base url

--- src/api/sensorConfig.js
+++ src/api/sensorConfig.js
@@ -1,4 +1,4 @@
-const BASE_URL = '/api/sensor-config';
+const BASE_URL = 'https://api.hydroleaf.se/api/sensor-config';
 
 export async function getSensorConfigs() {
     const res = await fetch(BASE_URL);
