5f66e29d796b4520294a4e50baffaa76d9fa1eef
Azad Bahramali <bahramali.az@gmail.com>
2025-09-03 20:18:06 +0200

    fixing issue with ui

--- src/pages/Live/components/NotesBlock.jsx
+++ src/pages/Live/components/NotesBlock.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './NotesBlock.module.css';
 import {bandMap, knownFields} from '../../common/dashboard.constants.js';
+import { useSensorConfig } from "../../../context/SensorConfigContext.jsx";
 
 function NotesBlock({ mergedDevices = {} }) {
   const metaFields = new Set(['timestamp', 'deviceId', 'compositeId', 'layer']);
