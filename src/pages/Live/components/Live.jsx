e9cbda54c23e15d408260a16e0f7207cdda09945
Azad <bahramali.az@gmail.com>
2025-10-31 11:38:37 +0100

    Remove live spectrum chart from live page

--- src/pages/Live/components/Live.jsx
+++ src/pages/Live/components/Live.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import SpectrumBarChart from "./SpectrumBarChart";
 import NotesBlock from "./NotesBlock";
 import TopicSection from "./TopicSection";
 import styles from "./Live.module.css";
 
 function Live({
-    sensorTopicDevices = {},
-    selectedDevice = "",
-    setSelectedDevice = () => {},
-    filteredCompositeIds = [],
-    sensorData = {},
     mergedDevices = {},
     systemTopics = {},
 }) {
@@ -20,34 +14,6 @@ function Live({
     return (
         <div className={styles.section}>
             <div className={styles.sectionBody}>
-                {/* Live spectrum chart for the selected device */}
-                {Object.keys(sensorTopicDevices).length > 0 && (
-                    <>
-                        <div className={styles.chartFilterRow}>
-                            <label className={styles.filterLabel}>
-                                Composite ID:
-                                <select
-                                    className={styles.intervalSelect}
-                                    value={selectedDevice}
-                                    onChange={(e) => setSelectedDevice(e.target.value)}
-                                >
-                                    {filteredCompositeIds.map((id) => (
-                                        <option key={id} value={id}>{id}</option>
-                                    ))}
-                                </select>
-                            </label>
-                        </div>
-
-                        <div className={styles.deviceLabel}>{selectedDevice}</div>
-
-                        {filteredCompositeIds.includes(selectedDevice) && (
-                            <div className={styles.spectrumBarChartWrapper}>
-                                <SpectrumBarChart sensorData={sensorData[selectedDevice]}/>
-                            </div>
-                        )}
-                    </>
-                )}
-
                 {/* Notes based on mergedDevices */}
                 <NotesBlock mergedDevices={mergedDevices}/>
 
