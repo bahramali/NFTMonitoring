d8d2f3959dd6e378992d6c732521276fc50347ab
Azad <bahramali.az@gmail.com>
2025-12-29 13:44:04 +0100

    Fix customer list search param loops

--- src/components/store/StoreLayout.jsx
+++ src/components/store/StoreLayout.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { useStorefront } from '../../context/StorefrontContext.jsx';
 import CartDrawer from './CartDrawer.jsx';
 import Toast from './Toast.jsx';
 import styles from './StoreLayout.module.css';
 
 export default function StoreLayout() {
-    const location = useLocation();
     const { isCartOpen, closeCart, toast, clearToast } = useStorefront();
 
     return (
         <div className={styles.shell}>
-            <main className={styles.main} key={location.key}>
+            <main className={styles.main}>
                 <div className={styles.content}>
                     <Outlet />
                 </div>
