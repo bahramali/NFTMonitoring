fa6e0cadb677d86057ae7e8036c492aa718628c8
Azad <bahramali.az@gmail.com>
2026-01-02 18:22:59 +0100

    Guard sensor config fetch on auth routes

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { SensorConfigProvider } from './context/SensorConfigContext.jsx';
 import { enableFetchLogging } from './utils/logFetch.js';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { StorefrontProvider } from './context/StorefrontContext.jsx';
@@ -14,12 +13,10 @@ if (shouldEnableFetchLogs) {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <SensorConfigProvider>
-            <AuthProvider>
-                <StorefrontProvider>
-                    <App />
-                </StorefrontProvider>
-            </AuthProvider>
-        </SensorConfigProvider>
+        <AuthProvider>
+            <StorefrontProvider>
+                <App />
+            </StorefrontProvider>
+        </AuthProvider>
     </React.StrictMode>,
 );
